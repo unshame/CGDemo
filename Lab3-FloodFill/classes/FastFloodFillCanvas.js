23052a77c14f665fed1c33a8cf620df3a11a42db
UnShame <mrunshame@gmail.com>
2018-09-26 23:08:57 +0300

    Lab3: fix file names, add console output

--- Lab3-FloodFill/classes/FastFloodFillCanvas.js
+++ Lab3-FloodFill/classes/FastFloodFillCanvas.js
@@ -1,33 +1,55 @@
-/* exported SlowFloodFillCanvas */
-class SlowFloodFillCanvas extends AbstractFloodFillCanvas {
+/* exported FastFloodFillCanvas */
+class FastFloodFillCanvas extends AbstractFloodFillCanvas {
 
     *fillGenerator(p) {
         let stack = [];
-
+        let numPushes = 0;
+        let numRepeats = 0;
+        let maxDepth = 0;
         let curColor = this.fillColor;
+
         if (this.renderer.pixelHasColor(p, curColor)) {
             return;
         }
+
         let prevColor = this.renderer.getPixelColor(p);
+        this.renderer.drawPixel(p, curColor);
         stack.push(p);
+        numPushes++;
+        maxDepth = 1;
 
         while(stack.length != 0) {
             let pp = stack.pop();
-            yield this.renderer.drawPixel(pp, [255, 0, 0, 255]);
-            yield this.renderer.drawPixel(pp, curColor);
 
             for(let i = 0; i < this.dirs.length; i++) {
+
                 let ppp = {
                     x: pp.x + this.dirs[i].x,
                     y: pp.y + this.dirs[i].y
                 };
 
-                if(this.renderer.pixelIsInside(ppp) && this.renderer.pixelHasColor(ppp, prevColor)) {
-                    stack.push(ppp);
+                if(!this.renderer.pixelIsInside(ppp)) {
+                    continue;
+                }
+
+                if (!this.renderer.pixelHasColor(ppp, prevColor)) {
+                    numRepeats++;
+                    continue;
+                }
+
+                yield this.renderer.drawPixel(ppp, [255, 0, 0, 255]);
+                yield this.renderer.drawPixel(ppp, curColor);
+                stack.push(ppp);
+
+                numPushes++;
+
+                if (stack.length > maxDepth) {
+                    maxDepth = stack.length;
                 }
             }
         }
 
+        console.log(`Fast algorithm: ${numPushes} stack pushes, ${numRepeats} already colored pixels checked, ${maxDepth} max stack depth.`);
         this.renderer.update();
     }
 
--- Lab3-FloodFill/classes/SlowFloodFillCanvas.js
+++ Lab3-FloodFill/classes/SlowFloodFillCanvas.js
@@ -1,33 +1,52 @@
-/* exported FastFloodFillCanvas */
-class FastFloodFillCanvas extends AbstractFloodFillCanvas {
+/* exported SlowFloodFillCanvas */
+class SlowFloodFillCanvas extends AbstractFloodFillCanvas {
 
     *fillGenerator(p) {
         let stack = [];
-
+        let numPushes = 0;
+        let numRepeats = 0;
+        let maxDepth = 0;
         let curColor = this.fillColor;
+
         if (this.renderer.pixelHasColor(p, curColor)) {
             return;
         }
+
         let prevColor = this.renderer.getPixelColor(p);
-        this.renderer.drawPixel(p, curColor);
         stack.push(p);
 
         while(stack.length != 0) {
             let pp = stack.pop();
+            yield this.renderer.drawPixel(pp, [255, 0, 0, 255]);
+            yield this.renderer.drawPixel(pp, curColor);
 
             for(let i = 0; i < this.dirs.length; i++) {
+
                 let ppp = {
                     x: pp.x + this.dirs[i].x,
                     y: pp.y + this.dirs[i].y
                 };
 
-                if(this.renderer.pixelIsInside(ppp) && this.renderer.pixelHasColor(ppp, prevColor)) {
-                    yield this.renderer.drawPixel(ppp, [255, 0, 0, 255]);
-                    yield this.renderer.drawPixel(ppp, curColor);
-                    stack.push(ppp);
+                if(!this.renderer.pixelIsInside(ppp)) {
+                    continue;
+                }
+
+                if (!this.renderer.pixelHasColor(ppp, prevColor)) {
+                    numRepeats++;
+                    continue;
+                }
+
+                stack.push(ppp);
+
+                numPushes++;
+
+                if(stack.length > maxDepth) {
+                    maxDepth = stack.length;
                 }
             }
         }
+
+        console.log(`Slow algorithm: ${numPushes} stack pushes, ${numRepeats} already colored pixels checked, ${maxDepth} max stack depth.`);
 
         this.renderer.update();
     }
