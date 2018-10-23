e928d8419aa3a9a80e47068898e9081981ac0e92
UnShame <mrunshame@gmail.com>
2018-10-23 18:53:57 +0300

    Lab7: multiline controls

--- Lab7-WebGL3D/classes/Geometry.js
+++ Lab7-WebGL3D/classes/Geometry.js
@@ -1,34 +1,34 @@
 /* exported Geometry */
 const Geometry = {
     makeTorus(r, sr, n, sn, k) {
-        // Temporary arrays for the vertices, normals and colors
-        var tv = [];
-        var tc = [];
+
+        var vertices = [];
+        var colors = [];
+
         // Iterates along the big circle and then around a section
         for (var i = 0; i < n; i++) { // Iterates over all strip rounds
             for (var j = 0; j < sn + 1 * (i == n - 1); j++) {// Iterates along the torus section
                 for (var v = 0; v < 2; v++) {// Creates zigzag pattern (v equals 0 or 1)
+
                     // Pre-calculation of angles
                     var a = 2 * Math.PI * (i + j / sn + k * v) / n;
                     var sa = 2 * Math.PI * j / sn;
                     var x, y, z;
+
                     // Coordinates on the surface of the torus
-                    tv.push(x = (r + sr * Math.cos(sa)) * Math.cos(a)); // X
-                    tv.push(y = (r + sr * Math.cos(sa)) * Math.sin(a)); // Y
-                    tv.push(z = sr * Math.sin(sa)); // Z
+                    vertices.push(x = (r + sr * Math.cos(sa)) * Math.cos(a)); // X
+                    vertices.push(y = (r + sr * Math.cos(sa)) * Math.sin(a)); // Y
+                    vertices.push(z = sr * Math.sin(sa)); // Z
+
                     // Colors
-                    tc.push(0.5 + 0.5 * x); // R
-                    tc.push(0.5 + 0.5 * y); // G
-                    tc.push(0.5 + 0.5 * z); // B
-                    tc.push(1.0); // Alpha
+                    colors.push(0.5 + 0.5 * x); // R
+                    colors.push(0.5 + 0.5 * y); // G
+                    colors.push(0.5 + 0.5 * z); // B
+                    colors.push(1.0); // Alpha
                 }
             }
         }
 
-        // Converts and returns array
-        return {
-            vertices: tv,
-            colors: tc
-        };
+        return {vertices, colors};
     }
 };
