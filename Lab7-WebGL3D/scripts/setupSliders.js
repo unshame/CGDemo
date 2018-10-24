94a3e4242199dd79db13e6fbb5c567a1cf52170d
UnShame <mrunshame@gmail.com>
2018-10-25 01:32:07 +0300

    Lab7: simplify setupSliders

--- Lab7-WebGL3D/scripts/setupSliders.js
+++ Lab7-WebGL3D/scripts/setupSliders.js
@@ -4,179 +4,179 @@ function setupSliders(renderer) {
     const minPos = -1500;
     const maxPos = 1500;
 
-    return {
+    let configs = {
 
-        x: setupSlider('#x', {
+        x: {
             value: renderer.translation[0],
             slide: (event, value) => renderer.translation[0] = value,
             min: minPos,
             max: maxPos,
             scrollStep: 10
-        }),
+        },
 
-        y: setupSlider('#y', {
+        y: {
             value: renderer.translation[1],
             slide: (event, value) => renderer.translation[1] = value,
             min: minPos,
             max: maxPos,
             scrollStep: 10
-        }),
+        },
 
-        z: setupSlider('#z', {
+        z: {
             value: renderer.translation[2],
             slide: (event, value) => renderer.translation[2] = value,
             min: minPos,
             max: maxPos,
             scrollStep: 10
-        }),
+        },
 
-        angleX: setupSlider('#angleX', {
+        angleX: {
             value: radToDeg(renderer.rotation[0]),
             slide: (event, value) => renderer.rotation[0] = degToRad(value),
             min: -180,
             max: 180,
-        }),
+        },
 
-        angleY: setupSlider('#angleY', {
+        angleY: {
             value: radToDeg(renderer.rotation[1]),
             slide: (event, value) => renderer.rotation[1] = degToRad(value),
             min: -180,
             max: 180,
-        }),
+        },
 
-        angleZ: setupSlider('#angleZ', {
+        angleZ: {
             value: radToDeg(renderer.rotation[2]),
             slide: (event, value) => renderer.rotation[2] = degToRad(value),
             min: -180,
             max: 180,
-        }),
+        },
 
-        amount: setupSlider('#amount', {
+        amount: {
             value: renderer.numObjects,
             slide: (event, value) => renderer.numObjects = value,
             min: 1,
             max: 50,
-        }),
+        },
 
-        spacing: setupSlider('#spacing', {
+        spacing: {
             value: renderer.sceneRadius,
             slide: (event, value) => renderer.sceneRadius = value,
             min: 0,
             max: 900,
-        }),
+        },
 
-        scale: setupSlider('#scale', {
+        scale: {
             value: renderer.scale[0],
             slide: (event, value) => renderer.scale.fill(value),
             min: -2,
             max: 2,
             step: 0.01,
             precision: 2,
-        }),
+        },
 
-        rotationX: setupSlider('#rotationX', {
+        rotationX: {
             value: radToDeg(renderer.objectRotationRate[0]),
             slide: (event, value) => renderer.objectRotationRate[0] = degToRad(value),
             min: -9,
             max: 9,
             scrollStep: 1
-        }),
+        },
 
-        rotationY: setupSlider('#rotationY', {
+        rotationY: {
             value: radToDeg(renderer.objectRotationRate[1]),
             slide: (event, value) => renderer.objectRotationRate[1] = degToRad(value),
             min: -9,
             max: 9,
             scrollStep: 1
-        }),
+        },
 
-        rotationZ: setupSlider('#rotationZ', {
+        rotationZ: {
             value: radToDeg(renderer.objectRotationRate[2]),
             slide: (event, value) => renderer.objectRotationRate[2] = degToRad(value),
             min: -9,
             max: 9,
             scrollStep: 1
-        }),
+        },
 
-        fov: setupSlider('#fov', {
+        fov: {
             value: radToDeg(renderer.fieldOfView),
             slide: (event, value) => renderer.fieldOfView = degToRad(value),
             min: 30,
             max: 160,
             name: 'FOV'
-        }),
+        },
 
-        camX: setupSlider('#camX', {
+        camX: {
             value: renderer.cameraTranslation[0],
             slide: (event, value) => renderer.cameraTranslation[0] = value,
             min: minPos,
             max: maxPos,
             name: 'OffsetX',
             scrollStep: 10
-        }),
+        },
 
-        camY: setupSlider('#camY', {
+        camY: {
             value: renderer.cameraTranslation[1],
             slide: (event, value) => renderer.cameraTranslation[1] = value,
             min: minPos,
             max: maxPos,
             name: 'OffsetY',
             scrollStep: 10
-        }),
+        },
 
-        camZ: setupSlider('#camZ', {
+        camZ: {
             value: renderer.cameraTranslation[2],
             slide: (event, value) => renderer.cameraTranslation[2] = value,
             min: minPos,
             max: maxPos,
             name: 'OffsetZ',
             scrollStep: 10
-        }),
+        },
 
-        camAngleX: setupSlider('#camAngleX', {
+        camAngleX: {
             value: radToDeg(renderer.cameraRotation[0]),
             slide: (event, value) => renderer.cameraRotation[0] = degToRad(value),
             min: -180,
             max: 180,
             name: 'CircleX'
-        }),
+        },
 
-        camAngleY: setupSlider('#camAngleY', {
+        camAngleY: {
             value: radToDeg(renderer.cameraRotation[1]),
             slide: (event, value) => renderer.cameraRotation[1] = degToRad(value),
             min: -180,
             max: 180,
             name: 'CircleY'
-        }),
+        },
 
-        targetX: setupSlider('#targetX', {
+        targetX: {
             value: renderer.targetTranslation[0],
             slide: (event, value) => renderer.targetTranslation[0] = value,
             min: minPos,
             max: maxPos,
             name: 'X',
             scrollStep: 10
-        }),
+        },
 
-        targetY: setupSlider('#targetY', {
+        targetY: {
             value: renderer.targetTranslation[1],
             slide: (event, value) => renderer.targetTranslation[1] = value,
             min: minPos,
             max: maxPos,
             name: 'Y',
             scrollStep: 10
-        }),
+        },
 
-        targetZ: setupSlider('#targetZ', {
+        targetZ: {
             value: renderer.targetTranslation[2],
             slide: (event, value) => renderer.targetTranslation[2] = value,
             min: minPos,
             max: maxPos,
             name: 'Z',
             scrollStep: 10
-        }),
+        },
 
-        torusR: setupSlider('#torusR', {
+        torusR: {
             value: torusProps[0],
             slide: (e, value) => {
                 torusProps[0] = value;
@@ -186,9 +186,9 @@ function setupSliders(renderer) {
             max: 500,
             name: 'Radius',
             scrollStep: 5
-        }),
+        },
 
-        torusSR: setupSlider('#torusSR', {
+        torusSR: {
             value: torusProps[1],
             slide: (e, value) => {
                 torusProps[1] = value;
@@ -198,9 +198,9 @@ function setupSliders(renderer) {
             max: 100,
             name: 'S Radius',
             scrollStep: 5
-        }),
+        },
 
-        torusStrips: setupSlider('#torusStrips', {
+        torusStrips: {
             value: torusProps[2],
             slide: (e, value) => {
                 torusProps[2] = value;
@@ -210,9 +210,9 @@ function setupSliders(renderer) {
             max: 50,
             name: 'Strips',
             scrollStep: 1
-        }),
+        },
 
-        torusSections: setupSlider('#torusSections', {
+        torusSections: {
             value: torusProps[3],
             slide: (e, value) => {
                 torusProps[3] = value;
@@ -222,9 +222,9 @@ function setupSliders(renderer) {
             max: 30,
             name: 'Sections',
             scrollStep: 1
-        }),
+        },
 
-        cylinderRadius: setupSlider('#cylinderRadius', {
+        cylinderRadius: {
             value: cylinderProps[0],
             slide: (e, value) => {
                 cylinderProps[0] = value;
@@ -234,9 +234,9 @@ function setupSliders(renderer) {
             max: 500,
             name: 'Radius',
             scrollStep: 5
-        }),
+        },
 
-        cylinderHeight: setupSlider('#cylinderHeight', {
+        cylinderHeight: {
             value: cylinderProps[1],
             slide: (e, value) => {
                 cylinderProps[1] = value;
@@ -246,9 +246,9 @@ function setupSliders(renderer) {
             max: 1000,
             name: 'Height',
             scrollStep: 10
-        }),
+        },
 
-        cylinderSides: setupSlider('#cylinderSides', {
+        cylinderSides: {
             value: cylinderProps[2],
             slide: (e, value) => {
                 cylinderProps[2] = value;
@@ -258,6 +258,13 @@ function setupSliders(renderer) {
             max: 50,
             name: 'Sides',
             scrollStep: 1
-        }),
+        },
     };
+
+    let sliders = {};
+    for(let key of Object.keys(configs)) {
+        sliders[key] = setupSlider('#' + key, configs[key]);
+    }
+
+    return sliders;
 }
