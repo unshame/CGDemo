fa2ca9a48ae1ed765c18fb6ed8c33c2630fa481e
UnShame <mrunshame@gmail.com>
2018-10-25 03:15:36 +0300

    Lab7: squished M4Math

--- Lab7-WebGL3D/classes/M4Math.js
+++ Lab7-WebGL3D/classes/M4Math.js
@@ -66,38 +66,15 @@ const M4Math = {
     },
 
     multiply: function(a, b) {
-        let a00 = a[0 * 4 + 0];
-        let a01 = a[0 * 4 + 1];
-        let a02 = a[0 * 4 + 2];
-        let a03 = a[0 * 4 + 3];
-        let a10 = a[1 * 4 + 0];
-        let a11 = a[1 * 4 + 1];
-        let a12 = a[1 * 4 + 2];
-        let a13 = a[1 * 4 + 3];
-        let a20 = a[2 * 4 + 0];
-        let a21 = a[2 * 4 + 1];
-        let a22 = a[2 * 4 + 2];
-        let a23 = a[2 * 4 + 3];
-        let a30 = a[3 * 4 + 0];
-        let a31 = a[3 * 4 + 1];
-        let a32 = a[3 * 4 + 2];
-        let a33 = a[3 * 4 + 3];
-        let b00 = b[0 * 4 + 0];
-        let b01 = b[0 * 4 + 1];
-        let b02 = b[0 * 4 + 2];
-        let b03 = b[0 * 4 + 3];
-        let b10 = b[1 * 4 + 0];
-        let b11 = b[1 * 4 + 1];
-        let b12 = b[1 * 4 + 2];
-        let b13 = b[1 * 4 + 3];
-        let b20 = b[2 * 4 + 0];
-        let b21 = b[2 * 4 + 1];
-        let b22 = b[2 * 4 + 2];
-        let b23 = b[2 * 4 + 3];
-        let b30 = b[3 * 4 + 0];
-        let b31 = b[3 * 4 + 1];
-        let b32 = b[3 * 4 + 2];
-        let b33 = b[3 * 4 + 3];
+        let a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3],
+            a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3],
+            a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3],
+            a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3],
+            b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3],
+            b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3],
+            b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3],
+            b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
+
         return [
             b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
             b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
@@ -156,55 +133,22 @@ const M4Math = {
     },
 
     inverse: function (m) {
-        let m00 = m[0 * 4 + 0];
-        let m01 = m[0 * 4 + 1];
-        let m02 = m[0 * 4 + 2];
-        let m03 = m[0 * 4 + 3];
-        let m10 = m[1 * 4 + 0];
-        let m11 = m[1 * 4 + 1];
-        let m12 = m[1 * 4 + 2];
-        let m13 = m[1 * 4 + 3];
-        let m20 = m[2 * 4 + 0];
-        let m21 = m[2 * 4 + 1];
-        let m22 = m[2 * 4 + 2];
-        let m23 = m[2 * 4 + 3];
-        let m30 = m[3 * 4 + 0];
-        let m31 = m[3 * 4 + 1];
-        let m32 = m[3 * 4 + 2];
-        let m33 = m[3 * 4 + 3];
-        let tmp_0 = m22 * m33;
-        let tmp_1 = m32 * m23;
-        let tmp_2 = m12 * m33;
-        let tmp_3 = m32 * m13;
-        let tmp_4 = m12 * m23;
-        let tmp_5 = m22 * m13;
-        let tmp_6 = m02 * m33;
-        let tmp_7 = m32 * m03;
-        let tmp_8 = m02 * m23;
-        let tmp_9 = m22 * m03;
-        let tmp_10 = m02 * m13;
-        let tmp_11 = m12 * m03;
-        let tmp_12 = m20 * m31;
-        let tmp_13 = m30 * m21;
-        let tmp_14 = m10 * m31;
-        let tmp_15 = m30 * m11;
-        let tmp_16 = m10 * m21;
-        let tmp_17 = m20 * m11;
-        let tmp_18 = m00 * m31;
-        let tmp_19 = m30 * m01;
-        let tmp_20 = m00 * m21;
-        let tmp_21 = m20 * m01;
-        let tmp_22 = m00 * m11;
-        let tmp_23 = m10 * m01;
+        let m00 = m[0 * 4 + 0], m01 = m[0 * 4 + 1], m02 = m[0 * 4 + 2], m03 = m[0 * 4 + 3],
+            m10 = m[1 * 4 + 0], m11 = m[1 * 4 + 1], m12 = m[1 * 4 + 2], m13 = m[1 * 4 + 3],
+            m20 = m[2 * 4 + 0], m21 = m[2 * 4 + 1], m22 = m[2 * 4 + 2], m23 = m[2 * 4 + 3],
+            m30 = m[3 * 4 + 0], m31 = m[3 * 4 + 1], m32 = m[3 * 4 + 2], m33 = m[3 * 4 + 3];
 
-        let t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
-            (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
-        let t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
-            (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
-        let t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
-            (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
-        let t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
-            (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+        let tmp_0 = m22 * m33, tmp_1 = m32 * m23, tmp_2 = m12 * m33, tmp_3 = m32 * m13,
+            tmp_4 = m12 * m23, tmp_5 = m22 * m13, tmp_6 = m02 * m33, tmp_7 = m32 * m03,
+            tmp_8 = m02 * m23, tmp_9 = m22 * m03, tmp_10 = m02 * m13, tmp_11 = m12 * m03,
+            tmp_12 = m20 * m31, tmp_13 = m30 * m21, tmp_14 = m10 * m31, tmp_15 = m30 * m11,
+            tmp_16 = m10 * m21, tmp_17 = m20 * m11, tmp_18 = m00 * m31, tmp_19 = m30 * m01,
+            tmp_20 = m00 * m21, tmp_21 = m20 * m01, tmp_22 = m00 * m11, tmp_23 = m10 * m01;
+
+        let t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) - (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+        let t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) - (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+        let t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) - (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+        let t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) - (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
 
         let d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
 
@@ -213,30 +157,18 @@ const M4Math = {
             d * t1,
             d * t2,
             d * t3,
-            d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
-                (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
-            d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
-                (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
-            d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
-                (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
-            d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
-                (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
-            d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
-                (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
-            d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
-                (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
-            d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
-                (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
-            d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
-                (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
-            d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
-                (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
-            d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
-                (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
-            d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
-                (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
-            d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
-                (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+            d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) - (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+            d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) - (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+            d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) - (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+            d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) - (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+            d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) - (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+            d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) - (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+            d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) - (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+            d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) - (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+            d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) - (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+            d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) - (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+            d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) - (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+            d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) - (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
         ];
     }
 };
